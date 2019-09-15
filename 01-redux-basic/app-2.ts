473452bd4683cd5a3e1d801d82598df667d2abaf
Alberto Garcia <albertoinformatica97@gmail.com>
2019-09-15 23:20:12 +0200

    First commit, Store creada

---
+++ 01-redux-basic/app-2.ts
@@ -0,0 +1,8 @@
+// import { reducer } from './contador/contador.reducer';
+// import { incrementadorAction, decrementadorAction, multiplicadorAction, divisorAction, resetAction } from './contador/contador.actions'
+
+// console.log( reducer(99, incrementadorAction) )
+// console.log( reducer(99, decrementadorAction) )
+// console.log( reducer(99, multiplicadorAction) )
+// console.log( reducer(99, divisorAction) )
+// console.log( reducer(99, resetAction) )
---
+++ 01-redux-basic/contador/contador.actions.ts
@@ -0,0 +1,19 @@
+import { Action } from '../ngrx-fake/ngrx'
+
+export const incrementadorAction:Action = {
+    type: 'INCREMENTAR'
+}
+export const decrementadorAction:Action = {
+    type: 'DECREMENTAR'
+}
+export const multiplicadorAction:Action = {
+    type: 'MULTIPLICAR',
+    payload: 9
+}
+export const divisorAction:Action = {
+    type: 'DIVIDIR',
+    payload: 9
+}
+export const resetAction:Action = {
+    type: 'RESET'
+}
