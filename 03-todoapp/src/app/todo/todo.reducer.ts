7da024db1292292b560ac1c7575947ab4326ff52
Alberto Garcia <albertoinformatica97@gmail.com>
2019-10-06 20:16:12 +0200

    Terminado video '44. Instalar NgRx, modelo, acciones y reducer de Todos'

---
+++ 03-todoapp/src/app/todo/todo.reducer.ts
@@ -0,0 +1,16 @@
+import * as fromTodo from './todo.actions'
+import { Todo } from './model/todo.model'
+
+const estadoInicial:Todo[] = []
+
+export function todoReducer(state = estadoInicial, action:fromTodo.Actions): Todo[] {
+
+  switch(action.type) {
+
+    case fromTodo.AGREGAR_TODO:
+      const todo = new Todo(action.texto)
+      return [...state, todo]
+
+  }
+
+}
