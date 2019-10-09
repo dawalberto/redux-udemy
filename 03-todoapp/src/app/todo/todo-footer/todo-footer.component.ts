d1fe7df90a44217cd0ba7a30ec6a421d66fdd09c
Alberto Garcia <albertoinformatica97@gmail.com>
2019-10-09 19:23:11 +0200

    Terminado video '56. Conteo de tareas pendientes'

--- 03-todoapp/src/app/todo/todo-footer/todo-footer.component.ts
+++ 03-todoapp/src/app/todo/todo-footer/todo-footer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
 import * as fromFilter from '../../filter/filter.actions';
+import { Todo } from '../model/todo.model';
 
 @Component({
   selector: 'app-todo-footer',
@@ -15,18 +16,26 @@ export class TodoFooterComponent implements OnInit {
 
   ngOnInit() {
     this.store.subscribe(state => {
+      this.countActives(state.todos)
       this.filterSelected = state.filter
     })
   }
 
+
   filtrosValidos:fromFilter.filtrosValidos[] = ['All', 'Completed', 'Active']
   filterSelected:fromFilter.filtrosValidos
+  actives:number
+
 
   changeFilter(filter:fromFilter.filtrosValidos) {
     this.filterSelected = filter
 
     const action = new fromFilter.SetFilterAction(filter)
     this.store.dispatch(action)
+  }
+
+  countActives(todos:Todo[]): void {
+    this.actives = todos.filter(todo => !todo.completado).length
   }
 
 }
